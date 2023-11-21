4be2f4d7d3edf06d19de4a355f80dfadf540feb5
mehakrafiq <mehak.rafiq@hotmail.com>
2023-11-21 21:42:59 +0500

    static variables initialized for each function

--- code/number_lines.py
+++ code/number_lines.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
-data = input()
+import os
+
+data = input("Enter file name: ")
+
+test_data = os.path.join("data", data) # call this variable in the function you make
 
 # Your code after this line - do not change anything above this line
